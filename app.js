32afd0da3604f145abbe582e53388151a3f4199b
JP2620 <saucedojuanpablo@gmail.com>
2020-07-26 12:24:47 -0300

    no se agrega 2 veces mismo socket

--- app.js
+++ app.js
@@ -43,12 +43,8 @@ io.sockets.on('connection', (socket) => {
       let otroPlayer = io.sockets.connected[idOtroP];
       otroPlayer.emit('disconnect_rival');
       otroPlayer.game = undefined;
-    } else if (SOCKET_WAIT_Q.getArray().includes(socket.id)) {
-      let array = SOCKET_WAIT_Q.getArray();
-      const index = array.indexOf(socket.id);
-      if (index > -1) {
-        array.splice(index, 1);
-      }
+    } else if (SOCKET_WAIT_Q.includes(socket.id)) {
+      SOCKET_WAIT_Q.remove(socket.id);
     }
   });
 
@@ -60,7 +56,7 @@ io.sockets.on('connection', (socket) => {
 
 setInterval(crearGame, 100);
 
-function Queue() { var a = [], b = 0;this.getArray = function() {return a} ;this.getLength = function () { return a.length - b }; this.isEmpty = function () { return 0 == a.length }; this.enqueue = function (b) { a.push(b) }; this.dequeue = function () { if (0 != a.length) { var c = a[b]; 2 * ++b >= a.length && (a = a.slice(b), b = 0); return c } }; this.peek = function () { return 0 < a.length ? a[b] : void 0 } };
+//function Queue() { var a = [], b = 0;this.getArray = function() {return a} ;this.getLength = function () { return a.length - b }; this.isEmpty = function () { return 0 == a.length }; this.enqueue = function (b) { a.push(b) }; this.dequeue = function () { if (0 != a.length) { var c = a[b]; 2 * ++b >= a.length && (a = a.slice(b), b = 0); return c } }; this.peek = function () { return 0 < a.length ? a[b] : void 0 } };
 function crearGame() {
   if (SOCKET_WAIT_Q.getLength() > 1) {
     let game = new TicTacToe(SOCKET_WAIT_Q.dequeue(), SOCKET_WAIT_Q.dequeue());
@@ -71,3 +67,81 @@ function crearGame() {
     io.sockets.connected[game.p2].game = game;
   } 
 }
+
+// Cola que no agrega repetidos
+function Queue(){
+
+  // initialise the queue and offset
+  var queue  = [];
+  var offset = 0;
+
+  // Returns the length of the queue.
+  this.getLength = function(){
+    return (queue.length - offset);
+  }
+
+  // Returns true if the queue is empty, and false otherwise.
+  this.isEmpty = function(){
+    return (queue.length == 0);
+  }
+
+  /* Enqueues the specified item. The parameter is:
+   *
+   * item - the item to enqueue
+   */
+  this.enqueue = function(item){
+    for (let i = 0; i < this.getLength(); i++) {
+      if (this.getArray()[i] === item) {
+        return;
+      }
+    } 
+    queue.push(item);
+  }
+
+  /* Dequeues an item and returns it. If the queue is empty, the value
+   * 'undefined' is returned.
+   */
+  this.dequeue = function(){
+
+    // if the queue is empty, return immediately
+    if (queue.length == 0) return undefined;
+
+    // store the item at the front of the queue
+    var item = queue[offset];
+
+    // increment the offset and remove the free space if necessary
+    if (++ offset * 2 >= queue.length){
+      queue  = queue.slice(offset);
+      offset = 0;
+    }
+
+    // return the dequeued item
+    return item;
+
+  }
+
+  /* Returns the item at the front of the queue (without dequeuing it). If the
+   * queue is empty then undefined is returned.
+   */
+  this.peek = function(){
+    return (queue.length > 0 ? queue[offset] : undefined);
+  }
+
+  this.getArray = function() {
+    return queue;
+  }
+
+  this.includes = function(elem) {
+    return this.getArray().includes(elem);
+  }
+
+  this.remove = function(elem) {
+    const index = queue.indexOf(elem);
+    if (index > -1) {
+      queue.splice(index, 1);
+    }
+  }
+
+
+}
+
